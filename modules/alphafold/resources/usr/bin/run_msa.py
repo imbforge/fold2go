7caeaa71afaaf7ff52b8ca46251118735adbad60
Patrick Hüther <huether@imb-mainz.de>
2024-02-20 18:16:10 +0100

    run_msa.py: use shutil.which to find executables

--- modules/alphafold/resources/usr/bin/run_msa.py
+++ modules/alphafold/resources/usr/bin/run_msa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import argparse
+import argparse, shutil
 
 from absl import logging
 from alphafold.data.pipeline import run_msa_tool
@@ -20,20 +20,18 @@ args = parser.parse_args()
 match args.database:
     case "uniref90":
         database_path=f'{args.database_root_path}/uniref90/uniref90.fasta'
-        msa_runner, msa_out_path, max_sto_sequences, msa_format = jackhmmer.Jackhmmer(binary_path='/usr/bin/jackhmmer', database_path=database_path), 'uniref90_hits.sto', 10000, 'sto'
+        msa_runner, msa_out_path, max_sto_sequences, msa_format = jackhmmer.Jackhmmer(binary_path=shutil.which('jackhmmer'), database_path=database_path), 'uniref90_hits.sto', 10000, 'sto'
     case "mgnify":
         database_path=f'{args.database_root_path}/mgnify/mgy_clusters_2022_05.fa'
-        msa_runner, msa_out_path, max_sto_sequences, msa_format = jackhmmer.Jackhmmer(binary_path='/usr/bin/jackhmmer', database_path=database_path), 'mgnify_hits.sto', 501, 'sto'
+        msa_runner, msa_out_path, max_sto_sequences, msa_format = jackhmmer.Jackhmmer(binary_path=shutil.which('jackhmmer'), database_path=database_path), 'mgnify_hits.sto', 501, 'sto'
     case "uniprot":
         database_path=f'{args.database_root_path}/uniprot/uniprot.fasta'
-        msa_runner, msa_out_path, max_sto_sequences, msa_format = jackhmmer.Jackhmmer(binary_path='/usr/bin/jackhmmer', database_path=database_path), 'uniprot_hits.sto', None, 'sto'
+        msa_runner, msa_out_path, max_sto_sequences, msa_format = jackhmmer.Jackhmmer(binary_path=shutil.which('jackhmmer'), database_path=database_path), 'uniprot_hits.sto', None, 'sto'
     case "bfd":
         databases=[f'{args.database_root_path}/uniref30/UniRef30_2021_03', f'{args.database_root_path}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt']
-        msa_runner, msa_out_path, max_sto_sequences, msa_format = hhblits.HHBlits(binary_path='/usr/bin/hhblits', databases=databases), 'bfd_uniref_hits.a3m', None, 'a3m'
+        msa_runner, msa_out_path, max_sto_sequences, msa_format = hhblits.HHBlits(binary_path=shutil.which('hhblits'), databases=databases), 'bfd_uniref_hits.a3m', None, 'a3m'
     case _:
         parser.error(f"{args.database} is not a valid choice")
 
 msa_runner.n_cpu = args.cores
 run_msa_tool(msa_runner=msa_runner, input_fasta_path=args.fasta_path, msa_out_path=msa_out_path, msa_format=msa_format, use_precomputed_msas=False, max_sto_sequences=max_sto_sequences)
-
-
