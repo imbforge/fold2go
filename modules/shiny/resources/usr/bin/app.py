70a793212190ec0ee93219aed3a1a70f1909f23f
Patrick Hüther <huether@imb-mainz.de>
2024-11-25 10:52:16 +0100

    shiny: various improvements
    * code refactoring and cleanup
    * store row selection in reactive.val to avoid invalidation on data
    * drop py3dmol dependency
    * add more tooltips to UI elements
      ingress
    * add sequence panel to mol* widget

--- modules/shiny/resources/usr/bin/app.py
+++ modules/shiny/resources/usr/bin/app.py
@@ -6,31 +6,38 @@ import plotly.express as px
 
 from ipymolstar import PDBeMolstar
 from pathlib import Path
-from shiny import reactive, req
+from shiny import reactive
 from shiny.express import input, render, ui
 from shinywidgets import render_plotly, render_widget
 
-# get environment and store vars
+## get environment and store vars
+
 njobs = int(os.getenv('SHINY_APP_NJOBS'))
 results_dir = Path(os.getenv("SHINY_APP_DATA")) / os.getenv("SHINY_APP_RUN_NAME")
 log_file = Path(os.getenv("SHINY_APP_LAUNCH_DIR")) / '.nextflow.log'
 
-# construct paths to model structure and confidence metrics
-# outputs are different for af2 and af3, so we need separate codepaths here
-def _construct_paths(record: dict) -> dict:
-    prediction = results_dir / 'predictions' / record.get('prediction_name')
-    if 'ranking_score' in record.keys():
-        # af3 output
-        return {
-            'structure': prediction / record.get('model_id') / 'model.cif',
-            'confidences' : prediction / record.get('model_id') / 'confidences.json'
-        }
+## define helper functions
+
+# load pae metric for selected prediction
+def _get_pae(record: dict) -> list:
+    if 'ranking_score' in record:
+        with (results_dir / 'predictions' / record.get('prediction_name') / record.get('model_id') / 'confidences.json').open() as fin:
+            return json.load(fin).get('pae')
     else:
-        # af2 output
-        return {
-            'structure': prediction / f"{record.get('model_rank')}.pdb",
-            'confidences': prediction / f"pae_{record.get('model_id')}.json"
-        }
+        with (results_dir / 'predictions' / record.get('prediction_name') / f"pae_{record.get('model_id')}.json").open() as fin:
+            return json.load(fin)[0].get('predicted_aligned_error')
+
+# load 3D model for selected prediction
+def _get_model(record: dict) -> dict:
+    if 'ranking_score' in record:
+        model = results_dir / 'predictions' / record.get('prediction_name') / record.get('model_id') / 'model.cif'
+    else:
+        model = results_dir / 'predictions' / record.get('prediction_name') / f"{record.get('model_rank')}.pdb"
+    return {
+        'data'  : model.read_text(),
+        'format': model.suffix[1:],
+        'binary': False
+    }
 
 # list files of completed predictions
 def _list_files() -> list:
@@ -40,14 +47,16 @@ def _list_files() -> list:
 # TODO: replace this with an http endpoint and use nf-weblog
 def _parse_log() -> list:
     msg = []
-    with open(log_file) as txt:
+    with log_file.open() as txt:
         for line in txt:
             if 'INFO  nextflow' in line:
                 msg.append(line.split(' - ')[-1])
     return msg
 
-# initialize reactive value to hold metrics dataframe
-df = reactive.value()
+## define reactive values and functions
+
+# initialize reactive values to hold metrics dataframe and selected row
+df, selection = reactive.value(), reactive.value()
 
 # construct data frame from list of files, check for changes every 30s
 @reactive.effect
@@ -58,10 +67,27 @@ def _():
         models.attrs['done'] = len(files)
         df.set(models)
 
+# store row selection
+@reactive.effect
+def _():
+    selected = render_frame.data_view(selected=True).to_dict(orient='records')
+    ui.update_tooltip("structure_tip", show=(not selected))
+    ui.update_tooltip("pae_tip", show=(not selected))
+    if selected:
+        selection.set(selected.pop())
+
+# terminate shiny server on button press
+@reactive.effect
+@reactive.event(input.terminate)
+def _():
+    return os.kill(os.getpid(), signal.SIGUSR1)
+
+## define user interface
+
 # display dataframe with calculated metrics and selectable rows
 with ui.card():
-    with ui.card_header():
-        with ui.tooltip(placement="right", id="metrics_tooltip"):
+    with ui.card_header(class_='text-center'):
+        with ui.tooltip(placement="bottom", id="metrics_tip"):
             "Metrics"
             "Results will appear here as they are produced"
     @render.data_frame
@@ -87,47 +113,45 @@ with ui.card():
     
             return render.DataGrid(df(), selection_mode='row')
 
-    @render.download(label="Download", filename="template_indep_info.csv")
-    def download_metrics():
-        yield df().to_csv(index=False)
+    with ui.card_footer(class_='text-center'):
+        # display button to download metrics table
+        with ui.tooltip(id="download_tip"):
+            @render.download(label="Download", filename="template_indep_info.csv")
+            def download_metrics():
+                yield df().to_csv(index=False)
+            "Press this button to download metrics table as csv"
 
-# render model structure based on selection in first card
-with ui.card():
-    with ui.card_header():
-        with ui.tooltip(placement="right", id="structure_tooltip"):
-            "Model structure"
-            "Select a row to display the corresponding 3D model"
-    @render_widget
-    def render_structure():
-        prediction = req(render_frame.data_view(selected=True).to_dict(orient='records')).pop()
-        structure = _construct_paths(prediction).get('structure')
-        return PDBeMolstar(
-            custom_data = {
-                'data'  : structure.read_bytes(),
-                'format': structure.suffix[1:],
-                'binary': False
-            },
-            alphafold_view = True
-        )
+        # display button to terminate shiny server process
+        with ui.tooltip(id="terminate_tip"):
+            ui.input_action_button("terminate", "Shutdown", class_="btn-danger")
+            "Press this button to terminate fold2go. Unfinished predictions will be lost"
 
-# plot predicted aligned error based on selection in first card
-with ui.card():
-    with ui.card_header():
-        with ui.tooltip(placement="right", id="pae_tooltip"):
-            "Predicted aligned error"
-            "Select a row to display the corresponding PAE plot"
-    @render_plotly
-    def render_pae():
-        prediction = req(render_frame.data_view(selected=True).to_dict(orient='records')).pop()
-        with open(_construct_paths(prediction).get('confidences')) as fin:
-            data = json.load(fin)
-        pae = data[0].get('predicted_aligned_error') if isinstance(data, list) else data.get('pae')
-        return px.imshow(pae, labels={'color': 'PAE'}, color_continuous_scale=px.colors.sequential.Viridis)
 
-# display button to terminate shiny server process
-with ui.card():
-    ui.input_action_button("terminate", "Shutdown", class_="btn-danger")
-    @reactive.effect
-    @reactive.event(input.terminate)
-    def _():
-        return os.kill(os.getpid(), signal.SIGUSR1)
+with ui.layout_columns(col_widths=(4,8)):
+    # plot predicted aligned error based on selection in first card
+    with ui.card():
+        with ui.card_header(class_='text-center'):
+            with ui.tooltip(placement="bottom", id="pae_tip"):
+                "Predicted Aligned Error"
+                "Select row to display corresponding PAE plot"
+        @render_plotly
+        def render_pae():
+            return px.imshow(
+                _get_pae(selection()),
+                labels = {'color': 'PAE'},
+                color_continuous_scale = px.colors.sequential.Viridis
+            )
+    # render model structure based on selection in first card
+    with ui.card():
+        with ui.card_header(class_='text-center'):
+            with ui.tooltip(placement="bottom", id="structure_tip"):
+                "Predicted Structure"
+                "Select row to display corresponding 3D structure"
+        @render_widget
+        def render_structure():
+            return PDBeMolstar(
+                custom_data = _get_model(selection()),
+                alphafold_view = True,
+                sequence_panel = True,
+                hide_animation_icon = True
+            )
